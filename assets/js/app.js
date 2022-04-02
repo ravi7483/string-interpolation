c7015032ae4e1a7eaad3d9219454142df33566af
ravi7483 <somwadvaibhav@gmail.com>
2022-04-02 09:55:42 +0530

    string interpolation
    main

---
+++ assets/js/app.js
@@ -0,0 +1,38 @@
+console.log("app.js file");
+
+var fname = "ravikiran";
+var lname = "niturkar";
+var result = fname + " " + lname ;
+console.log(result);
+
+var fname = "ravikiran";
+var lname = "niturkar"
+var from = "udgir";
+var profile = "mean stack developer";
+// hello there i'm ravikiran niturkar. from udgir profile mean stack developer. 
+var mainprofile = "Hello there i'm " + fname + " " + lname +". from " + from + " profile " + profile 
+console.log(mainprofile);
+
+var fname = "tony";
+var lname ="stark";
+var nickname = "iron man";
+var company = "stark indu";
+// Hello there, my name is tony stark nam to suna hi hoga, people call me iron and i'm the owner of strak indu.
+var IronmanIntro = "Hello there, my name is "  + fname + " " + lname + " to suna hi hoga" + ", people call me iron and i'm the owner of " + company + "."
+console.log(IronmanIntro);
+
+//Swami Vivekanand 12 January 1863  was an Indian Hindu monk and philosopher. He was a chief disciple of the 19th-century Indian mystic Ramakrishna.Influenced by Western esotericism, he was a key figure in the introduction of the Indian darsanas of Vedanta and Yoga to the Western world, and is credited with raising interfaith awareness, bringing Hinduism to the status of a major world religion during the late 19th century. He was a major force in the contemporary Hindu reform movements in India, and contributed to the concept of nationalism in colonial India. Vivekananda founded the Ramakrishna Math and the Ramakrishna Mission. He is perhaps best known for his speech which began with the words "Sisters and brothers of America . in which he introduced Hinduism at the Parliament of the World's Religions in Chicago in 1893.
+
+
+var fname = "swami";
+var lname = "vivekanand";
+var bron = 1863;
+var philosophy = "modern veanta raja yoga";
+var riligion = "Hinduism";
+var nationalism = "in colonial India";
+var founded = "Ramakrishna Math and the Ramakrishna Mission";
+var Chicago = "in 1893";
+
+var swami = fname + " " + lname + " 12 January " + bron + " was an Indian Hindu monk and " + philosophy + ". He was a chief disciple of the 19th-century " +  riligion + " mystic Ramakrishna. Influenced by Western esotericism, he was a key figure in the introduction of the Indian darsanas of " + philosophy + " to the Western world, and is credited with raising interfaith awareness, bringing " + riligion + " to the status of a major world religion during the late 19th century. He was a major force in the contemporary Hindu reform movements in India, and contributed to the concept of nationalism" + nationalism + ". Vivekananda founded the " + founded + ". He is perhaps best known for his speech which began with the words Sisters and brothers of America. in which he introduced Hinduism at the Parliament of the World's Religions in Chicago " + Chicago + "."
+
+console.log(swami);
